478c8b191014d67fe1ab7900bb19f6c5a9559ea2
eleonor2004 <eleonordjounkeng@gmail.com>
2025-08-05 10:29:19 +0100

    deployed successful , fixing baseUrl of the api

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -4,7 +4,8 @@ import axios from 'axios';
 const apiClient = axios.create({
   // Set the base URL for all API requests.
   // This means we can use relative paths like '/auth/token' later on.
-  baseURL: 'http://localhost:8001/api/v1',
+  //baseURL: 'http://localhost:8001/api/v1',
+  baseURL: 'https://synapse-backend-1.onrender.com',
   // Set default headers for all requests
   headers: {
     'Content-Type': 'application/json',
