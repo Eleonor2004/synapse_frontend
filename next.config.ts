7eafaae703c2e5564f10eff468ae3f427e4221d0
eleonor2004 <eleonordjounkeng@gmail.com>
2025-08-08 16:08:52 +0100

    not found page done

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,15 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async redirects() {
+    return [
+    {
+    source: '/',
+    destination: '/en',
+    permanent: true,
+    },
+    ]
+  },
 };
 
 export default withNextIntl(nextConfig);
